9b9d5d5435a1db285c6f554a5244bc2daa54e5a6
aydinmfatih <aydinmfatih@gmail.com>
2023-07-13 17:43:03 +0300

    Add project files.

---
+++ MVCDesignPattern/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCDesignPattern.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult GetProducts()
+        {
+
+            return View();
+        }
+    }
+}
