8b835feb2d1912e62a5f3dd052292e8fea1c0e8e
aydinmfatih <aydinmfatih@gmail.com>
2023-07-17 17:01:31 +0300

    updt

--- MVCDesignPattern/Controllers/ProductController.cs
+++ MVCDesignPattern/Controllers/ProductController.cs
@@ -1,19 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MVCDesignPattern.Models;
+using System.Collections.Generic;
 
 namespace MVCDesignPattern.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult GetProducts()
+        public IActionResult Index()
         {
-            var product = new Product();
-            return View();
+            var products = new List<Product> {
+             new Product { Id = 1 , ProductName="A Product", Quantity=19},
+             new Product { Id = 2, ProductName = "A Product", Quantity = 98 },
+             new Product { Id = 3, ProductName = "A Product", Quantity = 16 }
+                };
+            return View(products);
         }
-        [NonAction]
-        public void X()
-        {
-
-        }
+       
     }
 }
